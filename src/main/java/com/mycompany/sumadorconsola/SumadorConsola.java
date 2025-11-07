72cff7a0e5862390f16735a8fe05f2cf0c510310
Keiber Alejandro Rojas Garcia <Keiberale@outlook.es>
2025-11-07 08:57:38 -0500

    Tarea - Actualizacion 1: Version inicial del sumador

---
+++ src/main/java/com/mycompany/sumadorconsola/SumadorConsola.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.sumadorconsola;
+
+
+public class SumadorConsola {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 3;
+        int resultado = num1 + num2;
+
+        System.out.println("La suma es: " + resultado);
+    }
+}
